16ff1d8b93ba4449abd2ee562f51b75511012a71
Arnav Bhagwat <76836227+Thindino@users.noreply.github.com>
2023-04-24 00:43:00 +0530

    Update MutiplyTableRunner.java

--- MutiplyTableRunner.java
+++ MutiplyTableRunner.java
@@ -3,10 +3,11 @@ package com.FirstJavaProject;
 public class MutiplyTableRunner {
 
 	public static void main(String[] args) {
-		MultiplyTable table = new MultiplyTable(); // File_Name obj_name = new File_name();
-		// table.print(); // obj_name.Method_name(parameter);
+		MultiplyTable table = new MultiplyTable(); 
+		/* File_Name obj_name = new File_name();
+		  obj_name.Method_name(parameter);*/ 
 		table.print(3, 3, 5);
-		// table.print(3);
+		
 	}
 
 }

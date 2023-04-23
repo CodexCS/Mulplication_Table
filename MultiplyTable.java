ccbe161b9e74d9fd355cfd0805918d0b3e062f88
Arnav Bhagwat <76836227+Thindino@users.noreply.github.com>
2023-04-24 00:41:50 +0530

    Update MultiplyTable.java

--- MultiplyTable.java
+++ MultiplyTable.java
@@ -1,24 +1,10 @@
-package com.FirstJavaProject;
-
 public class MultiplyTable {
-	void print() {
-		print(5, 1, 10);
-	}
-
-	int b = 0;
-
-	void print(int b) {
-		print(b, 1, 10);
-
-	}
-
-	void print(int b, int from, int to) {
-			for( int a=from; a<=to; a++) {
-				System.out.printf("%d x %d = %d",a,b,a*b).println();
+	void print(int b, int from, int to) {	
+		for( int a=from; a<=to; a++) {
+			System.out.printf("%d x %d = %d",a,b,a*b).println();
 			}
 
 		}
-		
-
-
+	
 }
+
